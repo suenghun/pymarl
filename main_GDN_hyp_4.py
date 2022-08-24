f70ffd1339953f2cd8a748a06648d3bde0fdb84a
suenghun <63846187+suenghun@users.noreply.github.com>
2022-08-24 10:32:12 +0900

    Update main_GDN_hyp_4.py

--- main_GDN_hyp_4.py
+++ main_GDN_hyp_4.py
@@ -30,7 +30,7 @@ if sys.platform == "linux":
 
 
 regularizer = 0.8
-map_name = '2s_vs_1sc'
+map_name = '3s_vs_5z'
 
 
 
@@ -100,7 +100,7 @@ def main():
 
         gamma = 0.99
         epsilon = 1
-        learning_rate = 4e-4
+        learning_rate = 3e-4
         min_epsilon = 0.05
         anneal_steps = 50000
         n_multi_head = 1
